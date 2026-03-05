842fe5532515f7ae6e57ea82641bfdfbf91b8583
chablaouisoukainasolicode-create <chablaoui.soukaina.solicode@gmail.com>
2026-03-05 11:00:44 +0000

    Add files via upload

---
+++ exercice live coding/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+  <form method="post">
+    <label >nom</label>
+    <input type="text" name="NAME">
+    <label >prénom</label>
+    <input type="text" name="FNAME">
+    <label >age</label>
+    <input type="text" name="AGE">
+    <button type="submit" name="OK">ok</button>
+  </form>  
+  <?php
+  if(isset($_POST["OK"]));
+  $nom=($_POST($NAME));
+  $prénom=($_POST($FNAME));
+  $age=($_POST($AGE));
+if(empty($NAME)&&($FNAME)&&($AGE)&&(age>=18));
+ if(is_numeric($AGE));
+echo ($NAME).($FNAME).($AGE) .@ofppt.ma;
+
+  ?>
+</body>
+</html>
